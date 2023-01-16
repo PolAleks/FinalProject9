028892c057e8159f28995cc0a932b4af312f5d50
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-16 11:37:59 +0300

    Сортировка 5 фамилий

---
+++ Task_2/EnterFlagSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class EnterFlagSort
+    {
+        public delegate void EnterFlagSortDelegate(int flag);
+        public event EnterFlagSortDelegate FlagEnteredEvent;
+        public bool FlagEntered;
+        public void Read()
+        {
+            Console.WriteLine(new string('-',50));
+            Console.WriteLine("Выберите вариант сортировки:\n" +
+                              "1 - сортировка А-Я\n" +
+                              "2 - сортировка Я-А");
+            int flag = Convert.ToInt32(Console.ReadLine());
+
+            if (flag != 1 && flag != 2) throw new FormatException();
+
+            DataEntered(flag);
+
+            FlagEntered = true;
+        }
+
+        public virtual void DataEntered(int flag)
+        {
+            FlagEnteredEvent?.Invoke(flag);
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static string[] lastName = {"Иванов",
+                                    "Петров",
+                                    "Сидоров",
+                                    "Козлов",
+                                    "Краморов"};
+        static void Main(string[] args)
+        {
+            EnterFlagSort enFlagSort = new EnterFlagSort();
+            enFlagSort.FlagEnteredEvent += MethodSort;
+
+            while (!enFlagSort.FlagEntered)
+            {
+                try
+                {
+                    enFlagSort.Read();
+                }
+                catch (FormatException) 
+                {
+                    Console.WriteLine("Введено некорректное зачение!");
+                }
+            }
+
+            foreach (string lastName in lastName) 
+            {
+                Console.WriteLine(lastName);
+            }
+        }
+        static void MethodSort(int flag)
+        {
+            Array.Sort(lastName);
+            if (flag == 2) Array.Reverse(lastName);
+        }
+    }
+}
