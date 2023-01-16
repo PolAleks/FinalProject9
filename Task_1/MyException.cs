077810d7cbc8f3cbb63a88beacb03993a6062519
Александр Полушкин <aa.polushkin@yandex.ru>
2023-01-16 10:40:25 +0300

    Добавьте файлы проекта.

---
+++ Task_1/MyException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class MyException : Exception
+    {
+        public DateTime DateException { get; }
+        public MyException() { }
+        public MyException(string message, DateTime date) : base(message) 
+        {
+            DateException = date;
+        } 
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exception = { new MyException("Шефф, все пропало!", DateTime.Now),
+                                      new ArgumentOutOfRangeException("Вы ввели некорректное число!"),
+                                      new FileNotFoundException("Нет такого файла!"),
+                                      new DirectoryNotFoundException("Нет такой директории!"),
+                                      new IndexOutOfRangeException("Вы вышли за пределы массива!")};
+            foreach (Exception ex in exception)
+            {
+                try
+                {
+                    throw ex;
+                }
+                catch(MyException ex2) 
+                {
+                    Console.WriteLine($"{ex2.Message} Ахтунг ровно в {ex2.DateException}");
+                }
+                catch(ArgumentOutOfRangeException ex3)
+                {
+                    Console.WriteLine(ex3.ParamName);
+                }
+                catch(Exception ex4)
+                {
+                    Console.WriteLine(ex4.Message);
+                }
+                finally
+                {
+                    Console.WriteLine(new String('-', 100));
+                }
+            }
+        }
+    }
+}
